4bc1eaf34e4238a14d4b7db504f8fa783e6b3ca3
Silin Chen <silin.chen05@gmail.com>
2020-08-15 23:45:37 +0930

    followed the YouTube tutorial to complete the Angular8

---
+++ CoreAngular8/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreAngular8.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreAngular8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/<StudentsController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            List<Student> oStudents = new List<Student>() { 
+                 new Student(){Id = 1, Name = "Shakib", Roll = 1001},
+                 new Student(){Id = 2, Name = "Ratin", Roll = 1002},
+                 new Student(){Id = 3, Name = "Rahat", Roll = 1003}
+            };
+            return oStudents;
+        }
+
+        // GET api/<StudentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
